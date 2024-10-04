46e6dadd6a4325f09eb4e2881eed56e97afbb611
MohammadmehdiKhani <mm.y.khani@gmail.com>
2024-10-04 15:10:12 +0330

    countries controller is added

---
+++ HoteListing API/Migrations/20241004092500_makingCountryHotelsFieldNotRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HoteListing_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class makingCountryHotelsFieldNotRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
